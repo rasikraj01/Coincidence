63a27d4c5c680fe3feff1b09fc98b8a8d9847f17
rasikraj01 <rasikraj01@gmail.com>
2019-05-18 02:20:26 +0530

    tweaks

--- plgcheck/forms.py
+++ plgcheck/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class CheckForm(forms.Form):
-	 cnt1 = forms.CharField(label='Enter Source Code 1')
-	 cnt2 = forms.CharField(label='Enter Source Code 2')
+	 cnt1 = forms.CharField(label='Enter Source Code 1', widget=forms.Textarea)
+	 cnt2 = forms.CharField(label='Enter Source Code 2', widget=forms.Textarea)
